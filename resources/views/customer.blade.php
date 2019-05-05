a70a0fe19df308a620738c4472335bb18ee51b8d
Blancafort5 <polblancafortrivas@gmail.com>
2019-05-06 01:11:01 +0200

    Customers and services views

--- resources/views/customer.blade.php
+++ resources/views/customer.blade.php
@@ -1,32 +1,148 @@
 @extends('main')
 @section('customer')
   @if(empty($id))
-    <table class="table table-bordered table-dark">
-  <thead>
-    <tr>
-      <th scope="col">Cliente</th>
-      <th scope="col">Servicio</th>
-      <th scope="col">Fecha</th>
-    </tr>
-  </thead>
-  <tbody>
-      @foreach($customers as $customer)
-        <tr>
-          <th scope="row"><a href="{{ route('customer', $customer->id ) }}">{{$customer->name}}
-          {{$customer->lastname}}</a></th>     
-          <td>{{$customer->serviceName}}</td>   
-          <td>{{$customer->day}}</td>
-        </tr>   
-      @endforeach    
-  </tbody>
-</table>    
-  
+    <div class="container">
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">Cliente</th>
+            <th scope="col">Servicio</th>
+            <th scope="col">Fecha</th>
+          </tr>
+        </thead>
+        <tbody>
+          @foreach($customers as $customer)
+            <tr>
+              <th scope="row"><a href="{{ route('customer', $customer->id ) }}">{{$customer->name}}
+              {{$customer->lastname}}</a></th>     
+              <td>{{$customer->serviceName}}</td>   
+              <td>{{$customer->day}}</td>
+            </tr>   
+          @endforeach    
+        </tbody>
+      </table>    
+    </div>  
   @endif
 @endsection
+
+
 @section('customerDetail')
   @if(!empty($id))
-   <div>
-     <label><?php dd($customers)?></label>
-  </div>
+    <br><br>
+    <div class="container">
+      <form class="form-horizontal">
+        <div class="form-group">
+          <label class="control-label col-sm-2" for="name">Nombre:</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control" id="name" name="name">
+          </div>
+        </div>
+        <div class="form-group">
+          <label class="control-label col-sm-2" for="lastname">Apellidos:</label>
+          <div class="col-sm-10">          
+            <input type="text" class="form-control" id="lastname" name="lastname">
+          </div>
+        </div>
+        <div class="form-group">
+          <label class="control-label col-sm-2" for="phone">Teléfono:</label>
+          <div class="col-sm-10">          
+            <input type="tel" class="form-control" id="phone" name="phone">
+          </div>
+        </div>
+        <div class="form-group">
+          <label class="control-label col-sm-2" for="email">Email:</label>
+          <div class="col-sm-10">
+            <input type="email" class="form-control" id="email" name="email">
+          </div>
+        </div>
+        <div class="form-group">
+          <label class="control-label col-sm-2" for="gender">Género:</label>
+          <div class="col-sm-10">          
+            <input type="text" class="form-control" id="gender" name="gender">
+          </div>
+        </div>
+        <div class="form-group">
+          <label class="control-label col-sm-2" for="postalCode">Código postal:</label>
+          <div class="col-sm-10">          
+            <input type="number" class="form-control" id="postalCode" name="postalCode" min="1" max="99999">
+          </div>
+        </div>
+        <div class="form-group">
+          <label class="control-label col-sm-2" for="rightImage">Derechos de imagen:</label>
+          <div class="col-sm-10">          
+            <label><input type="checkbox" name="rightImage"> </label>
+          </div>
+        </div>
+        <div class="form-group">
+          <label class="control-label col-sm-2" for="birthdate">Fecha de nacimiento:</label>
+          <div class="col-sm-10">          
+            <input type="date" class="form-control" id="birthdate" name="birthdate">
+          </div>
+        </div>
+        <div class="form-group">
+          <label class="control-label col-sm-2" for="observations">Obsevaciones:</label>
+          <div class="col-sm-10">          
+            <textarea class="form-control" rows="5" id="comment" name="comment"></textarea>
+          </div>
+        </div>
+        <div class="form-group">
+          <label class="control-label col-sm-2" for="creationDate">Fecha de alta:</label>
+          <div class="col-sm-10">          
+            <input type="date" class="form-control" id="creationDate" name="creationDate">
+          </div>
+        </div>
+      </form>
+    </div>
+
+    <br><br>
+    <div class="container">
+      <ul class="nav nav-tabs">
+        <li class="active"><a href="#">Ficha color</a></li>
+        <li><a href="#">Ficha general</a></li>
+      </ul>
+    </div>
+  @endif  
+@endsection
+
+
+@section('service')
+  @if(!empty($id))
+    <br><br>
+    <div class="container">
+      <form class="form-horizontal">
+        <div class="form-group">
+          <label class="control-label col-sm-2" for="name">Nombre servicio:</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control" id="name" name="name">
+          </div>
+        </div>
+        <div class="form-group">
+          <label class="control-label col-sm-2" for="duration">Duración:</label>
+          <div class="col-sm-10">          
+            <input type="number" class="form-control" id="duration" name="duration">
+          </div>
+        </div>
+        <div class="form-group">
+          <label class="control-label col-sm-2" for="price">Precio:</label>
+          <div class="col-sm-10">          
+            <input type="number" class="form-control" id="price" name="price">
+          </div>
+        </div>
+        <div class="form-group">
+          <label class="control-label col-sm-2" for="creationDate">Fecha de alta:</label>
+          <div class="col-sm-10">          
+            <input type="date" class="form-control" id="creationDate" name="creationDate">
+          </div>
+        </div>
+      </form>
+    </div>
+
+    <br><br>
+    <div class="container">
+      <ul class="nav nav-tabs">
+        <li class="active"><a href="#">Ficha color</a></li>
+        <li><a href="#">Ficha general</a></li>
+      </ul>
+    </div>
   @endif  
 @endsection
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,42 +1,46 @@
 <html>
-<head>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.bundle.min.js">
-</head>
-<body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
-  <div class="container">
-    <a class="navbar-brand" href="#">
-          <img src="http://placehold.it/150x50?text=Logo" alt="">
-        </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-    <div class="collapse navbar-collapse" id="navbarResponsive">
-      <ul class="navbar-nav ml-auto">
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('customer') }}">Clientes</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('appoinmentForm') }}">Citas</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('productForm') }}">Productos</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pedidos</a>
-        </li>
-      </ul>
+
+  <head>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <link rel="stylesheet" type="text/javascript" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.bundle.min.js">
+    <link rel="stylesheet" type="text/css"        href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css"        href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  </head>
+
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+    <div class="container">
+      <a class="navbar-brand" href="#">
+            <img src="http://placehold.it/150x50?text=Logo" alt="">
+          </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('customer') }}">Clientes</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('appoinmentForm') }}">Citas</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('productForm') }}">Productos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pedidos</a>
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
-</nav>
-@yield('customer')
-@yield('customerDetail')
-@yield('appoinmentForm')
-@yield('productForm')
+  </nav>
+  @yield('customer')
+  @yield('customerDetail')
+  @yield('service')
+  @yield('appoinmentForm')
+  @yield('productForm')
 
 
-</body>
+  </body>
 
 </html>
